9227e583a27c0c9d194862cbd7df4cc9ac12039a
EllDoesStuff <pylarinos20297451@student.nhvweb.net>
2025-09-24 16:18:13 +0000

    Creating Datatypes Lesson

---
+++ lesson01_datatypes.py
@@ -0,0 +1,31 @@
+#This is a comment, they're quite useful
+
+#STRINGS
+print("hello world")
+#Uses quotation marks to run text just as... well... text!
+print("Eat \nmy shorts, \nSkinner!")
+print("Name \tBilly \nAge: \t300 \nGrade: \t9")
+
+#INTEGERS
+print(42)
+print(-23)
+
+#FLOATS
+print(4.0)
+print(2.0 + 4) #makes a float
+print(2.0 + 4.5) #this too
+
+#B O O L E A N (true or false)
+
+print(True)
+print(False)
+print(True+False)
+print(1==1)
+print(1!=1)
+print("Hello"=="Hello")
+print(5+5==11)
+
+print(type("To see this message, bundle me in a good 'ol fashun'd print!"))
+print(type(9))
+print(type(4.0))
+print(type(True))
