c7cfca8dfb751d686482aa7f7d64fff7c788b17d
EllDoesStuff <pylarinos20297451@student.nhvweb.net>
2025-09-25 17:28:44 +0000

    finish

---
+++ lesson02_variables.py
@@ -0,0 +1,55 @@
+Password = "Orenthall25"
+Email = "Elltificer@gmail.com"
+print("Email:",Email)
+print("Password:", Password)
+
+Tempurature=42.9
+type(print(Tempurature))
+
+enabled=False
+is_complete=True
+print(is_complete)
+
+x="njcfoibhgewugbfiah"
+y="evilguy@gmail.com"
+print("Password:", x)
+print("Email:", y)
+
+#for math problems
+s=78
+e=9
+print(s+e)
+
+#variables can be updated
+count=10
+print(count)
+count_down = count-1
+print(count_down)
+count=count_down
+print(count)
+
+name="Mandelbröt Sett"
+age=21
+job= "Rockstar"
+
+print(name)
+print(age)
+print(job)
+
+count1=10
+print(count1)
+count_up=count1+5
+print(count_up)
+
+x=4
+y="hello"
+temp=y
+y=x
+x=temp
+print("y=",y)
+print("x=",x)
+
+is_raining=False
+print("Is it raining? ", is_raining)
+is_raining=True
+print("Is it raining? ", is_raining)
