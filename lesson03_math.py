8493238b496fefadf8a1c29589d0832cfe522343
EllDoesStuff <pylarinos20297451@student.nhvweb.net>
2025-09-26 18:00:12 +0000

    Finishd

---
+++ lesson03_math.py
@@ -0,0 +1,40 @@
+#math operators: +, -, *, /, , //, %, **
+
+add = 7+2
+print("Sum:", add)
+
+subtract= 7-6
+print("Difference:", subtract)
+
+multiply= 7*6 
+print("Product:", multiply)
+
+float_divide= 6/7
+print("Float Divison:", float_divide)
+
+integer_divide= 4//2
+print("Integer Division:", integer_divide)
+
+modulus= 7%2
+print("Modulus:", modulus)
+
+exponent=7**2
+print("Exponent:", exponent)
+
+print(2+2==10-6)
+
+result1=(2*3)*4
+print("Result 1:",result1)
+
+result2=2**3*4
+print("Result 2:", result2)
+
+result3=20/5*2
+print("Result 3:", result3)
+#returns float
+
+result4=10-2+3
+print("Result 4:", result4)
+
+result5=5+2**3*(4-1)
+print("Result 5:", result5)
